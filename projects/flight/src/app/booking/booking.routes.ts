b1d95086314d444ab0c1f6a079ccb39d9205ef74
mikezks <23002661+mikezks@users.noreply.github.com>
2024-06-10 14:22:27 +0200

    refactor: functional resolver, activated route for form hydration

--- projects/flight/src/app/booking/booking.routes.ts
+++ projects/flight/src/app/booking/booking.routes.ts
@@ -1,11 +1,11 @@
+import { importProvidersFrom } from '@angular/core';
 import { Routes } from '@angular/router';
-import { FlightBookingComponent, FlightEditComponent, FlightSearchComponent } from './feature-flight';
-import { FlightResolver } from './logic-flight';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
+import { FlightBookingComponent, FlightEditComponent, FlightSearchComponent } from './feature-flight';
+import { resolveFlight } from './logic-flight';
 import { TicketEffects } from './logic-flight/+state/effects';
 import { ticketFeature } from './logic-flight/+state/reducer';
-import { importProvidersFrom } from '@angular/core';
 
 
 export const BOOKING_ROUTES: Routes = [
@@ -40,7 +40,7 @@ export const BOOKING_ROUTES: Routes = [
             path: 'edit/:id',
             component: FlightEditComponent,
             resolve: {
-              flight: FlightResolver
+              flight: resolveFlight
             }
           }
         ]
