195b2ca6c119dab924a01ddd947657eb94550acf
mikezks <23002661+mikezks@users.noreply.github.com>
2024-06-10 13:12:02 +0200

    refactor: app routing root level standalone

--- projects/flight/src/app/app.config.ts
+++ projects/flight/src/app/app.config.ts
@@ -2,24 +2,27 @@ import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http"
 import { ApplicationConfig, importProvidersFrom } from "@angular/core";
 import { EffectsModule } from "@ngrx/effects";
 import { StoreModule } from "@ngrx/store";
-import { AppRoutingModule } from "./app-routing.module";
 import { routerFeature } from "./shared/logic-router-state";
 import { SharedModule } from "./shared/shared.module";
 import { UiCoreModule } from "./shared/ui-core/ui-core.module";
+import { PreloadAllModules, provideRouter, withPreloading } from "@angular/router";
+import { APP_ROUTES } from "./app.routes";
 
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideRouter(APP_ROUTES,
+      withPreloading(PreloadAllModules)
+    ),
+    provideHttpClient(
+      withInterceptorsFromDi()
+    ),
     importProvidersFrom(
-      AppRoutingModule,
       StoreModule.forRoot(),
       EffectsModule.forRoot(),
       StoreModule.forFeature(routerFeature),
       UiCoreModule,
       SharedModule
     ),
-    provideHttpClient(
-      withInterceptorsFromDi()
-    )
   ]
 };
