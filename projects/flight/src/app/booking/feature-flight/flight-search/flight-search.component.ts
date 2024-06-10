62f0a708e5e534d483d506795700e4645ca9ed4f
mikezks <23002661+mikezks@users.noreply.github.com>
2024-06-10 10:27:12 +0200

    refactor: smart component standalone flight search

--- projects/flight/src/app/booking/feature-flight/flight-search/flight-search.component.ts
+++ projects/flight/src/app/booking/feature-flight/flight-search/flight-search.component.ts
@@ -1,11 +1,20 @@
+import { AsyncPipe, JsonPipe, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { Flight, FlightFilter } from '../../logic-flight';
+import { FlightCardComponent } from '../../ui-flight/flight-card/flight-card.component';
+import { UiFlightModule } from '../../ui-flight/ui-flight.module';
 import { TicketsFacade } from './../../logic-flight/+state/facade';
 
 
 @Component({
   selector: 'app-flight-search',
   templateUrl: './flight-search.component.html',
+  standalone: true,
+  imports: [
+    NgIf, NgFor, JsonPipe, AsyncPipe,
+    UiFlightModule,
+    FlightCardComponent
+  ]
 })
 export class FlightSearchComponent {
   protected filter = {
