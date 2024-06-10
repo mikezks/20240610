a636705bcc3a85d1116d02c776b4145e4fed8915
mikezks <23002661+mikezks@users.noreply.github.com>
2024-06-10 10:59:28 +0200

    refactor: routing standalone

--- projects/flight/src/app/booking/booking.module.ts
+++ projects/flight/src/app/booking/booking.module.ts
@@ -3,25 +3,19 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
-import { BookingRoutingModule } from './booking-routing.module';
 import { FlightBookingComponent, FlightEditComponent } from './feature-flight';
 import { TicketEffects } from './logic-flight/+state/effects';
 import { ticketFeature } from './logic-flight/+state/reducer';
 import { UiFlightModule } from './ui-flight/ui-flight.module';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
-  declarations: [
-    FlightBookingComponent,
-    FlightEditComponent
-  ],
-  imports: [
-    CommonModule,
-    BookingRoutingModule,
-    ReactiveFormsModule,
-    StoreModule.forFeature(ticketFeature),
-    EffectsModule.forFeature([TicketEffects]),
-    UiFlightModule
-  ]
+    imports: [
+
+        UiFlightModule,
+        FlightBookingComponent,
+        FlightEditComponent
+    ]
 })
 export class BookingModule { }
